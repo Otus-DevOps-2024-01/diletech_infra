4334f94de0b16e6d3e11e21c068d55725df6cba6
Your Name <you@example.com>
2024-03-14 06:14:24 +0300

    cloud-testapp try fix

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -5,5 +5,5 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongod
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/' /etc/mongod.conf # для прохождения теста
-sudo systemctl start mongod
-sudo systemctl enable mongod
+sudo service mongod start || sudo systemctl start mongod
+sudo service mongod enable || sudo systemctl enable mongod
