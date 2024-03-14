60d64bc946b34ad39ccacb1884139683309c5e33
Your Name <you@example.com>
2024-03-14 06:07:28 +0300

    cloud-testapp try fix

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+sudo apt-get update && sudo apt-get install apt-transport-https
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
